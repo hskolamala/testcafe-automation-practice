ef9ea57e9724ae2959bed679b1fd76763e1a4708
Krishna Kolamala <107515203+hskolamala@users.noreply.github.com>
2022-09-13 14:36:50 -0700

    added more tests

--- tests/novacoreApi/test1.api.test.js
+++ tests/novacoreApi/test1.api.test.js
@@ -2,7 +2,43 @@ import { t } from "testcafe";
 
 fixture("novacore api's");
 
-test("novacore-api-test", async (t) => {
+test("novacore-api-test1", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("novacore-api-test2", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("novacore-api-test3", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("novacore-api-test4", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("novacore-api-test5", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("novacore-api-test6", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("novacore-api-test7", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("novacore-api-test8", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("novacore-api-test9", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("novacore-api-test10", async (t) => {
   const response = await t.request(`https://reqres.in/api/users/2`);
   console.log(response.status);
 });
