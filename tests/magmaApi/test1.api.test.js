c40d3cff5dea763ac227adc7c9aa4e5c77ef0924
Krishna Kolamala <107515203+hskolamala@users.noreply.github.com>
2022-09-13 14:35:50 -0700

    added more tests

--- tests/magmaApi/test1.api.test.js
+++ tests/magmaApi/test1.api.test.js
@@ -14,3 +14,31 @@ test("magma-api-test3", async (t) => {
   const response = await t.request(`https://reqres.in/api/users/2`);
   console.log(response.status);
 });
+test("magma-api-test4", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("magma-api-test5", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("magma-api-test6", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("magma-api-test7", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("magma-api-test8", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("magma-api-test9", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("magma-api-test10", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
