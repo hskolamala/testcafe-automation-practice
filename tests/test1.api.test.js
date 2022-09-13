bf20c29c0322b370e0e84c87c814b7c3b6ddc283
Krishna Kolamala <107515203+hskolamala@users.noreply.github.com>
2022-09-13 14:37:58 -0700

    added more tests

--- tests/test1.api.test.js
+++ tests/test1.api.test.js
@@ -2,7 +2,43 @@ import { t } from "testcafe";
 
 fixture("testcafe demo");
 
-test("my-first-test", async (t) => {
+test("my-first-test1", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("my-first-test2", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("my-first-test3", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("my-first-test4", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("my-first-test5", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("my-first-test6", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("my-first-test7", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("my-first-test8", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("my-first-test9", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("my-first-test10", async (t) => {
   const response = await t.request(`https://reqres.in/api/users/2`);
   console.log(response.status);
 });
