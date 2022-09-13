ead811bc89e9cbfe919fe6a7b5920f8e4bbefc41
kkolamala <kkolamala1984@gmail.com>
2022-09-13 10:28:39 -0700

    added more tests to magmaApi

--- tests/magmaApi/test1.api.test.js
+++ tests/magmaApi/test1.api.test.js
@@ -2,7 +2,15 @@ import { t } from "testcafe";
 
 fixture("magma api's");
 
-test("magma-api-test", async (t) => {
+test("magma-api-test1", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("magma-api-test2", async (t) => {
+  const response = await t.request(`https://reqres.in/api/users/2`);
+  console.log(response.status);
+});
+test("magma-api-test3", async (t) => {
   const response = await t.request(`https://reqres.in/api/users/2`);
   console.log(response.status);
 });
